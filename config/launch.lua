77237f24e8ad5fd2436e277687343b201424aa48
elliotxx <951376975@qq.com>
2024-05-13 18:49:22 +0800

    refactor: set default_prog = pwsh on windows

--- config/launch.lua
+++ config/launch.lua
@@ -6,7 +6,7 @@ local options = {
 }
 
 if platform.is_win then
-    options.default_prog = { 'powershell' }
+    options.default_prog = { 'pwsh' }
     options.launch_menu = {
         { label = 'PowerShell Core',    args = { 'pwsh' } },
         { label = 'PowerShell Desktop', args = { 'powershell' } },
